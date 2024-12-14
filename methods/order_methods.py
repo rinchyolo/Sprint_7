c8a100d16196000d3903712196fb0ffdb91ad2e9
Михалевская Карина Анатольевна <KMikhalevskaya@alfabank.ru>
2024-12-14 23:14:36 +0300

    Добавила аннотации

--- methods/order_methods.py
+++ methods/order_methods.py
@@ -1,12 +1,14 @@
+import allure
 import requests
 
 from config import BASE_URL, CREATE_ORDER, ORDER_LIST
 
 
 class OrderMethods:
-
+    @allure.step('Вызов метода POST для создания заказа')
     def post_order(self, params):
         return requests.post(f'{BASE_URL}{CREATE_ORDER}', json=params)
 
+    @allure.step('Вызов метода GET для получение списка заказов')
     def get_orders(self, courier_id):
         return requests.get(f'{BASE_URL}{ORDER_LIST}{courier_id}')
