52a215402043ac3d427821c205652d45ab9cf449
Михалевская Карина Анатольевна <KMikhalevskaya@alfabank.ru>
2024-12-14 12:10:11 +0300

    Сократила код

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-ORDER_DATA_1 = {
+ORDER_BASE_DATA = {
     "firstName": "Naruto",
     "lastName": "Uchiha",
     "address": "Konoha, 142 apt.",
@@ -7,49 +7,14 @@ ORDER_DATA_1 = {
     "rentTime": 5,
     "deliveryDate": "2020-06-06",
     "comment": "Saske, come back to Konoha",
-    "color": [
-        "BLACK"
-    ]
 }
 
-ORDER_DATA_2 = {
-    "firstName": "Naruto",
-    "lastName": "Uchiha",
-    "address": "Konoha, 142 apt.",
-    "metroStation": 4,
-    "phone": "+7 800 355 35 35",
-    "rentTime": 5,
-    "deliveryDate": "2020-06-06",
-    "comment": "Saske, come back to Konoha",
-    "color": [
-        "GREY"
-    ]
-}
-
-ORDER_DATA_3 = {
-    "firstName": "Naruto",
-    "lastName": "Uchiha",
-    "address": "Konoha, 142 apt.",
-    "metroStation": 4,
-    "phone": "+7 800 355 35 35",
-    "rentTime": 5,
-    "deliveryDate": "2020-06-06",
-    "comment": "Saske, come back to Konoha",
-    "color": [
-        "GREY",
-        "BLACK"
-    ]
-}
-ORDER_DATA_4 = {
-    "firstName": "Naruto",
-    "lastName": "Uchiha",
-    "address": "Konoha, 142 apt.",
-    "metroStation": 4,
-    "phone": "+7 800 355 35 35",
-    "rentTime": 5,
-    "deliveryDate": "2020-06-06",
-    "comment": "Saske, come back to Konoha"
-}
+ORDER_DATA_VARIANTS = [
+    {**ORDER_BASE_DATA, "color": ["BLACK"]},
+    {**ORDER_BASE_DATA, "color": ["GREY"]},
+    {**ORDER_BASE_DATA, "color": ["GREY", "BLACK"]},
+    ORDER_BASE_DATA.copy()
+]
 
 COURIER_EXIST_MESSAGE = 'Этот логин уже используется. Попробуйте другой.'
 CREATE_ACCOUNT_NOT_ENOUGH_DATA = 'Недостаточно данных для создания учетной записи'
--- tests/orders/test_create_order.py
+++ tests/orders/test_create_order.py
@@ -1,16 +1,12 @@
 import allure
 import pytest
 
-from data import ORDER_DATA_1, ORDER_DATA_2, ORDER_DATA_3, ORDER_DATA_4
+from data import ORDER_DATA_VARIANTS
 from methods.order_methods import OrderMethods
 
 
 class TestCreateOrder:
-    @pytest.mark.parametrize(
-        "payload", [
-            ORDER_DATA_1, ORDER_DATA_2, ORDER_DATA_3, ORDER_DATA_4
-        ]
-    )
+    @pytest.mark.parametrize("payload", ORDER_DATA_VARIANTS)
     @allure.title("Создание заказа")
     def test_create_order(self, payload):
         response = OrderMethods().post_order(payload)
